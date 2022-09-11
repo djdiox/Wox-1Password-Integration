e869bda35f72d64ce939dc22b6aa39c43b37d529
diox <markuswagner93@gmail.com>
2022-09-11 06:57:18 +0200

    added Wox Plugin and 1PasswordProvider + Settings

---
+++ Providers/1PasswordProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.Providers
+{
+    internal class _1Password
+    {
+    }
+}
